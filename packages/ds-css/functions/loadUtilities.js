abdfa0953a74855ece397dbf340bfb6644130e33
ZecKa <18116930+zecka@users.noreply.github.com>
2025-09-26 06:01:02 +0200

    rename utility to tw-utility to avoid confusion with daisyui

--- packages/ds-css/functions/loadUtilities.js
+++ packages/ds-css/functions/loadUtilities.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
 export const loadUtilities = () => {
-    const utilitiesDir = fs.readdirSync('./src/utilities')
+    const utilitiesDir = fs.readdirSync('./src/tw-utilities')
     let utilitiesContent = ''
     for (const file of utilitiesDir) {
         if (file.endsWith('.css')) {
-            const content = fs.readFileSync(`./src/utilities/${file}`, 'utf-8')
+            const content = fs.readFileSync(`./src/tw-utilities/${file}`, 'utf-8')
             utilitiesContent += `\n${content}\n`
         }
     }
